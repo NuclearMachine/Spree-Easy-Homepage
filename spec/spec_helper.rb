031ef897e91e540908b3e19802b6a477292c3ef9
Jibril Tapiador <tapiador@jibril.ch>
2019-12-28 22:47:38 +0800

    modifications to spec_helper, adds shoulda_matchers gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
-# Run Coverage report
 require 'simplecov'
 SimpleCov.start do
   add_filter 'spec/dummy'
@@ -10,20 +9,22 @@ SimpleCov.start do
   add_group 'Libraries', 'lib/spree'
 end
 
-# Configure Rails Environment
 ENV['RAILS_ENV'] = 'test'
 
-require File.expand_path('../dummy/config/environment.rb', __FILE__)
+begin
+  require File.expand_path('../dummy/config/environment', __FILE__)
+rescue LoadError
+  puts 'Could not load dummy application. Please ensure you have run `bundle exec rake test_app`'
+  exit
+end
 
 require 'rspec/rails'
 require 'ffaker'
 require 'pry'
+require 'shoulda/matchers'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 
-# Requires factories and other useful helpers defined in spree_core.
 require 'spree/testing_support/authorization_helpers'
 require 'spree/testing_support/capybara_ext'
 require 'spree/testing_support/controller_requests'
@@ -31,36 +32,21 @@ require 'spree/testing_support/factories'
 require 'spree/testing_support/url_helpers'
 
 RSpec.configure do |config|
-  # Infer an example group's spec type from the file location.
-  config.infer_spec_type_from_file_location!
+  config.mock_with :rspec
+  config.use_transactional_fixtures = false
+  config.fail_fast = ENV['FAIL_FAST'] || false
 
-  # == URL Helpers
-  #
-  # Allows access to Spree's routes in specs:
-  #
-  # visit spree.admin_path
-  # current_path.should eql(spree.products_path)
+  config.infer_spec_type_from_file_location!
   config.include Spree::TestingSupport::UrlHelpers
 
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  config.mock_with :rspec
+  config.raise_errors_for_deprecations!
+  
   config.color = true
   config.default_formatter = 'doc'
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # Capybara javascript drivers require transactional fixtures set to false, and we use DatabaseCleaner
-  # to cleanup after each test instead.  Without transactional fixtures set to false the records created
-  # to setup a test will be unavailable to the browser, which runs under a separate server instance.
-  config.use_transactional_fixtures = false
-
-  config.fail_fast = ENV['FAIL_FAST'] || false
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end  
+  
   config.order = 'random'
 end
