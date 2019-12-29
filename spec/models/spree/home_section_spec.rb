e595c26b77c06c254183aa9cc83d64bbf918c6f0
Jibril Tapiador <tapiador@jibril.ch>
2019-12-29 13:21:42 +0800

    home_section_spec quick modifications for association check

--- spec/models/spree/home_section_spec.rb
+++ spec/models/spree/home_section_spec.rb
@@ -2,15 +2,16 @@ require 'spec_helper'
 
 describe Spree::HomeSection do
   describe 'associations' do
-    it { is_expected.to have_many(:home_section_products) }
+    it { is_expected.to have_many(:home_section_products).dependent(:destroy) }
     it { is_expected.to have_many(:products).through(:home_section_products)}
   end
 
-  describe 'after_create' do
+  describe 'after create' do
     let!(:section) { build(:section) }
 
     it 'should act as a list' do
       expect(section).to respond_to(:set_list_position)
+      
     end
   end
 end
