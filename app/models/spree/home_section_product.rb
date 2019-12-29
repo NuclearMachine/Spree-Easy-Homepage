7155c481429623f0fb873f67421d3867ea8136c1
Jibril Tapiador <tapiador@jibril.ch>
2019-12-29 13:20:26 +0800

    associations should be required for home_section_product

--- app/models/spree/home_section_product.rb
+++ app/models/spree/home_section_product.rb
@@ -2,7 +2,7 @@ module Spree
   class HomeSectionProduct < Spree::Base
     acts_as_list
 
-    belongs_to :home_section, class_name: 'Spree::HomeSection'
-    belongs_to :product, class_name: 'Spree::Product'
+    belongs_to :home_section, class_name: 'Spree::HomeSection', required: true
+    belongs_to :product, class_name: 'Spree::Product', required: true
   end
 end
