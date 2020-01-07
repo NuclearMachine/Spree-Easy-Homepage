ae62829796ce26ac0f99b55a1b5f58858cbcc7aa
Jibril Tapiador <tapiador@jibril.ch>
2020-01-07 19:57:50 +0800

    adds new behavior for the HomeSection model

--- app/models/spree/home_section.rb
+++ app/models/spree/home_section.rb
@@ -1,8 +1,29 @@
 module Spree
   class HomeSection < Spree::Base
+    include Spree::ProductIdsProcessable
     acts_as_list
 
     has_many :home_section_products, dependent: :destroy
     has_many :products, through: :home_section_products, source: :product
+
+    after_save :reset_section_products
+
+    attr_accessor :product_ids
+
+    def delete_sections
+      home_section_products.delete_all
+    end
+
+    def has_product_ids?
+      product_ids.present?
+    end
+
+    protected
+
+    def reset_section_products
+      return unless has_product_ids?
+      delete_sections
+      add_products(product_ids: product_ids)
+    end
   end
 end
--- app/models/spree/home_section_product.rb
+++ app/models/spree/home_section_product.rb
@@ -1,6 +1,6 @@
 module Spree
   class HomeSectionProduct < Spree::Base
-    acts_as_list
+    acts_as_list scope: :home_section
 
     belongs_to :home_section, class_name: 'Spree::HomeSection', required: true
     belongs_to :product, class_name: 'Spree::Product', required: true
