2ead1f16848ab454b8235eba96823cc9cc71cbbe
Jibril Tapiador <tapiador@jibril.ch>
2020-01-08 13:03:18 +0800

    adds methods for getting the first image in a Product

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -3,6 +3,16 @@ module Spree::ProductDecorator
     base.has_many :home_section_products, class_name: 'Spree::HomeSectionProduct'
     base.has_many :home_sections, through: :home_section_products, class_name: 'Spree::HomeSection'
   end
+
+  def image_url(style = :product)
+    image = images.first
+    return unless image.present?
+    image.url(style)
+  end
+
+  def has_images?
+    images.any?
+  end
 end
 
 Spree::Product.prepend Spree::ProductDecorator
