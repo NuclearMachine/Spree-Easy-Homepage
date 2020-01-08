25901264294468d51321787bd5b52de8989e0ac8
Jibril Tapiador <tapiador@jibril.ch>
2020-01-08 15:01:05 +0800

    adds specs for ProductPresenter

---
+++ spec/presenters/spree/product_presenter_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe Spree::ProductPresenter do
+  let(:product_presenter) { described_class.new(product: product, main_app: main_app) }
+  let(:product) { create(:product) { |product| product.images.create(attributes_for(:image)) } }
+
+  describe 'public interface' do
+    context 'when parse is invoked' do
+      it 'parses without raising any errors' do
+        expect { product_presenter.parse }.not_to raise_error
+      end
+
+      it 'returns the correct hash of product attributes' do
+        product_hash = product_presenter.parse
+        expect(product_hash[:id]).to eq(product.id)
+        expect(product_hash[:name]).to eq(product.name)
+        expect(product_hash[:path]).not_to be_empty
+        expect(product_hash[:image]).not_to be_empty
+      end
+    end
+  end
+end
+
+private
+
+def main_app
+  Rails.application.class.routes.url_helpers
+end
